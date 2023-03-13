3aa77bfb341abd1643ea5ad8c1d4ce51f6628dd0
Paul Matros <paul.matros@gmail.com>
2023-03-13 10:57:31 +0100

    naming convention unified

--- src/main/java/com/github/arena/challenges/weakmdparser/MarkdownParser.java
+++ src/main/java/com/github/arena/challenges/weakmdparser/MarkdownParser.java
@@ -3,8 +3,8 @@ package com.github.arena.challenges.weakmdparser;
 public class MarkdownParser {
     private boolean activeList = false;
 
-    String parse(String markdown) {
-        String[] lines = markdown.split("\n");
+    String parse(String parsedString) {
+        String[] lines = parsedString.split("\n");
         StringBuilder result = new StringBuilder();
         for (String currentLine : lines) {
             result.append(parseLine(currentLine));
@@ -12,77 +12,77 @@ public class MarkdownParser {
         return result.toString();
     }
 
-    private String parseLine(String currentLine) {
-        currentLine = parseHeader(currentLine);
-        currentLine = parseList(currentLine);
-        currentLine = parseParagraph(currentLine);
-        currentLine = parseListContainer(currentLine);
-        return parseFontStyles(currentLine);
+    private String parseLine(String parsedLine) {
+        parsedLine = parseHeader(parsedLine);
+        parsedLine = parseList(parsedLine);
+        parsedLine = parseParagraph(parsedLine);
+        parsedLine = parseListContainer(parsedLine);
+        return parseFontStyles(parsedLine);
     }
 
-    private String parseListContainer(String currentLine) {
+    private String parseListContainer(String parsedLine) {
         if (!activeList) {
-            if (isList(currentLine)) {
+            if (isList(parsedLine)) {
                 activeList = true;
-                return "<ul>" + currentLine;
+                return "<ul>" + parsedLine;
             }
         } else {
             activeList = false;
-            return currentLine + "</ul>";
+            return parsedLine + "</ul>";
         }
-        return currentLine;
+        return parsedLine;
     }
 
-    private String parseHeader(String markdown) {
+    private String parseHeader(String parsedLine) {
         int count = 0;
 
-        for (int i = 0; i < markdown.length() && markdown.charAt(i) == '#'; i++) {
+        for (int i = 0; i < parsedLine.length() && parsedLine.charAt(i) == '#'; i++) {
             count++;
         }
 
         if (count == 0) {
-            return markdown;
+            return parsedLine;
         }
 
-        return "<h" + count + ">" + markdown.substring(count + 1) + "</h" + count + ">";
+        return "<h" + count + ">" + parsedLine.substring(count + 1) + "</h" + count + ">";
     }
 
-    private String parseList(String markdown) {
-        if (markdown.startsWith("*")) {
-            String skipAsterisk = markdown.substring(2);
+    private String parseList(String parsedLine) {
+        if (parsedLine.startsWith("*")) {
+            String skipAsterisk = parsedLine.substring(2);
             return "<li>" + skipAsterisk + "</li>";
         }
-        return markdown;
+        return parsedLine;
     }
 
-    private String parseParagraph(String markdown) {
-        if (!(isHeader(markdown) || isList(markdown)))
-            return "<p>" + markdown + "</p>";
-        return markdown;
+    private String parseParagraph(String parsedLine) {
+        if (!(isHeader(parsedLine) || isList(parsedLine)))
+            return "<p>" + parsedLine + "</p>";
+        return parsedLine;
     }
 
-    private String parseFontStyles(String markdown) {
-        String result = parseBold(markdown);
+    private String parseFontStyles(String parsedLine) {
+        String result = parseBold(parsedLine);
         return parseItalic(result);
     }
 
-    private String parseBold(String markdown) {
+    private String parseBold(String parsedLine) {
         String lookingFor = "__(.+)__";
         String update = "<strong>$1</strong>";
-        return markdown.replaceAll(lookingFor, update);
+        return parsedLine.replaceAll(lookingFor, update);
     }
 
-    private String parseItalic(String markdown) {
+    private String parseItalic(String parsedLine) {
         String lookingFor = "_(.+)_";
         String update = "<em>$1</em>";
-        return markdown.replaceAll(lookingFor, update);
+        return parsedLine.replaceAll(lookingFor, update);
     }
 
-    private boolean isList(String markdown) {
-        return markdown.matches("(<li>).*");
+    private boolean isList(String parsedLine) {
+        return parsedLine.matches("(<li>).*");
     }
 
-    private boolean isHeader(String markdown) {
-        return markdown.matches("(<h).*");
+    private boolean isHeader(String parsedLine) {
+        return parsedLine.matches("(<h).*");
     }
 }
